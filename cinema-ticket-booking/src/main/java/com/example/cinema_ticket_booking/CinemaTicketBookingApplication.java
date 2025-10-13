6c6b1863b15c877c2a95ce6a686aac9f566aa687
Yaroslav Los <yarroslavlos@gmail.com>
2025-10-13 13:57:59 +0300

    Initial commit

---
+++ cinema-ticket-booking/src/main/java/com/example/cinema_ticket_booking/CinemaTicketBookingApplication.java
@@ -0,0 +1,13 @@
+package com.example.cinema_ticket_booking;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class CinemaTicketBookingApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(CinemaTicketBookingApplication.class, args);
+	}
+
+}
